ceb86c8d6ea078584f2f29c9c039acbba950c2d5
Jeeva Kalaiselvam <77735712+jeevakalaiselvam@users.noreply.github.com>
2021-06-13 10:27:51 +0530

    tabs working

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 "use strict";
 
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+const nav = document.querySelector(".nav");
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+
+///////////////////////////////////////
+// Modal window
 
 const openModal = function (e) {
     e.preventDefault();
@@ -19,7 +25,7 @@ const closeModal = function () {
     overlay.classList.add("hidden");
 };
 
-btnsOpenModal.forEach((button) => button.addEventListener("click", openModal));
+btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
 
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
@@ -28,4 +34,68 @@ document.addEventListener("keydown", function (e) {
     if (e.key === "Escape" && !modal.classList.contains("hidden")) {
         closeModal();
     }
+});
+
+///////////////////////////////////////
+// Button scrolling
+btnScrollTo.addEventListener("click", function (e) {
+    const s1coords = section1.getBoundingClientRect();
+    console.log(s1coords);
+
+    console.log(e.target.getBoundingClientRect());
+
+    console.log("Current scroll (X/Y)", window.pageXOffset, window.pageYOffset);
+
+    console.log(
+        "height/width viewport",
+        document.documentElement.clientHeight,
+        document.documentElement.clientWidth
+    );
+
+    // Scrolling
+    // window.scrollTo(
+    //   s1coords.left + window.pageXOffset,
+    //   s1coords.top + window.pageYOffset
+    // );
+
+    // window.scrollTo({
+    //   left: s1coords.left + window.pageXOffset,
+    //   top: s1coords.top + window.pageYOffset,
+    //   behavior: 'smooth',
+    // });
+
+    section1.scrollIntoView({ behavior: "smooth" });
+});
+
+//PAGE NAGIVATION USING EVENT DELEGATION
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+    e.preventDefault();
+
+    // Matching strategy
+    if (e.target.classList.contains("nav__link")) {
+        const id = e.target.getAttribute("href");
+        document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+    }
+});
+
+//Tabbed Components
+tabsContainer.addEventListener("click", (e) => {
+    const clicked = e.target.closest(".operations__tab");
+    //Return if container is clicked and nothing inside it is clicked
+    if (!clicked) return;
+
+    //Reset active data
+
+    tabs.forEach((tab) => tab.classList.remove("operations__tab--active"));
+    tabsContent.forEach((tab) =>
+        tab.classList.remove("operations__content--active")
+    );
+
+    //Make the clicked tab active
+    clicked.classList.add("operations__tab--active");
+
+    //Activating content area by using the data attribute
+    document
+        .querySelector(`.operations__content--${clicked.dataset.tab}`)
+        .classList.add("operations__content--active");
 });
